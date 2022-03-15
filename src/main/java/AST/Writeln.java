1f203081317367e374372fe5d3594a5a4d5ffc71
Max Blennemann <23maxb@students.harker.org>
2022-03-15 14:57:28 -0700

    added expression

--- src/main/java/AST/Writeln.java
+++ src/main/java/AST/Writeln.java
@@ -2,5 +2,20 @@ package AST;
 
 public class Writeln extends Statement
 {
+private String Expression;
+
+public Writeln(String e)
+{
+    e = Expression;
+}
+
+public void exec()
+{
+    System.out.println(new eva)
+}
 
 }
+
+
+
+
