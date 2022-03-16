151187b912a609d3f4ae923f8e6f5b25c21ada9f
Max Blennemann <23maxb@students.harker.org>
2022-03-15 17:42:05 -0700

    block implemented

--- src/main/java/AST/Block.java
+++ src/main/java/AST/Block.java
@@ -1,11 +1,22 @@
 package AST;
 
+import java.util.List;
+
 public class Block extends Statement
 {
+    private List<Statement> statements;
+
+    public Block(List<Statement> statements)
+    {
+        this.statements = statements;
+    }
 
     //TODO: implement this!
     public void exec(Environment e)
     {
-
+        for (Statement statement : statements)
+        {
+            statement.exec(e);
+        }
     }
 }
